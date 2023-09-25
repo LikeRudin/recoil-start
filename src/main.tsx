69d3150143bba5ecb849054a7882ef26f0799de7
Young Jin <likeRudin@gmail.com>
2023-09-25 17:18:30 +0900

    add theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={darkTheme}>
     <App />
-  </React.StrictMode>,
-)
+  </ThemeProvider>
+);
