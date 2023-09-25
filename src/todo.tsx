fce5c2e62243a8e995ba684e5f6e49df01aeb758
Young Jin <likeRudin@gmail.com>
2023-09-25 23:34:55 +0900

    Functionalities: changing state of todo

--- src/todo.tsx
+++ src/todo.tsx
@@ -1,3 +1,8 @@
+import styled from "styled-components";
+import { useSetRecoilState } from "recoil";
+import { TodosAtom } from "./todo-atom";
+const Button = styled.button``;
+
 interface TodoProps {
   text: string;
   id: string;
@@ -5,11 +10,31 @@ interface TodoProps {
 }
 
 export const Todo = ({ text, id, category }: TodoProps) => {
+  const categories = ["TODO", "DOING", "DONE"];
+  const setTodoCategory = useSetRecoilState(TodosAtom);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const name = event.currentTarget.name as TodoProps["category"];
+    setTodoCategory((todos) => {
+      const [targetTodo] = todos.filter(({ id: todoId }) => id === todoId);
+      return [
+        ...todos.filter(({ id: todoId }) => id !== todoId),
+        { ...targetTodo, category: name },
+      ];
+    });
+  };
+
   return (
     <>
       <li key={id}>
         {category} : {text}
       </li>
+      {categories
+        .filter((kind) => kind !== category)
+        .map((kind) => (
+          <Button name={kind} onClick={handleClick}>
+            {kind}
+          </Button>
+        ))}
     </>
   );
 };
