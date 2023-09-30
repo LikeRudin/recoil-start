374d3b98a23b7997cea6286b73599bde78aeb43e
Young Jin <likeRudin@gmail.com>
2023-10-01 01:54:06 +0900

    FUNCTIONALITY: Creating new Bars

---
+++ src/components/create-bar.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import { useSetRecoilState } from "recoil";
+import { IBar, dataState } from "../atoms";
+
+interface FormCreatingBarProps {
+  boardIndex: number;
+  listIndex: number;
+}
+
+const Form = styled.form``;
+const Input = styled.input``;
+
+const FormCreatingBar = ({ boardIndex, listIndex }: FormCreatingBarProps) => {
+  const setDatas = useSetRecoilState(dataState);
+  const { register, setValue, handleSubmit } = useForm();
+  const onValid = ({ text }: IBar) => {
+    setDatas((oldDatas) => {
+      const newDatas = JSON.parse(JSON.stringify(oldDatas));
+      newDatas[boardIndex]["lists"][listIndex]["bars"].unshift({
+        text,
+        id: Date.now(),
+      });
+      return newDatas;
+    });
+    setValue("text", "");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onValid)}>
+      <Input
+        {...register("text", { required: "please type bar", minLength: 1 })}
+        placeholder="create new Bar"
+      />
+    </Form>
+  );
+};
+
+export default FormCreatingBar;
