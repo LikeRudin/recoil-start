8a2ecf56004de2805d6ea7211a3c96132f24d34f
Young Jin <likeRudin@gmail.com>
2023-09-30 20:34:49 +0900

    FUNCTIONALITY: change atoms

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -38,3 +38,43 @@ export const boardState = atom<IBoardState>({
     내일: ["TODO", "DOING", "DONE"],
   },
 });
+
+export const dataState = atom({
+  key: "datas",
+  default: [
+    {
+      오늘: [
+        { TODO: [{ id: 1, text: "코딩" }] },
+        {
+          DOING: [
+            { id: 11, text: "잠자기" },
+            { id: 21, text: "게임하기" },
+          ],
+        },
+        {
+          DONE: [
+            { id: 111, text: "샤워하기" },
+            { id: 211, text: "밥먹기" },
+          ],
+        },
+      ],
+    },
+    {
+      내일: [
+        { TODO: [{ id: 2, text: "코딩" }] },
+        {
+          DOING: [
+            { id: 21, text: "잠자기" },
+            { id: 22, text: "게임하기" },
+          ],
+        },
+        {
+          DONE: [
+            { id: 221, text: "샤워하기" },
+            { id: 222, text: "밥먹기" },
+          ],
+        },
+      ],
+    },
+  ],
+});
